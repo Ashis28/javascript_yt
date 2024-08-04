36b9d9aabb05d833edf07efdd1406d30b759683a
Ashis <116480505+Ashis28@users.noreply.github.com>
2024-08-04 06:38:44 +0000

    comparison done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -32,3 +32,22 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(typeof stringNumber)
 console.log(stringNumber)
+
+console.log("**operations**");
+
+let value = 12
+let negValue = -value
+console.log(negValue)
+console.log(typeof negvalue)
+
+let str1 = "hello";
+let str2 = " abhi";
+console.log(str1+str2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+let t1 = 1+2+'3';
+console.log(typeof t1)
+
+console.log(+true)
+console.log(+"")
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+console.log("2">1);
+console.log("2">4);
+//js auto matically converted string to int hrere
+
+console.log(null>0)
+console.log(null == 0)
+console.log(null >=0 )
+//comparison convert null to 0 i.e. line 7 
+// == works differently
+
+console.log(undefined == 0)  //allways give false
+
+//strict check ===
+console.log("2"===2);  //it checks the datatype also
+console.log("2"==2)
